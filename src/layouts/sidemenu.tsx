b2f27fe7d8b48fbb46ef75cb8633645feadb9769
ItsMeShrewt <im.shrewt@gmail.com>
2025-03-05 22:17:44 +0800

    New Project

---
+++ src/layouts/sidemenu.tsx
@@ -0,0 +1,69 @@
+
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
+
+function Sidemenu() {
+return (
+<>
+    <aside className="app-sidebar" id="sidebar">
+      <div className="main-sidebar-header">
+        <a href="/" className="header-logo"></a>
+      </div>
+      <div className="main-sidebar" id="sidebar-scroll">
+        <nav className="main-menu-container nav nav-pills flex-col sub-open">
+          <div className="slide-left" id="slide-left">
+          </div>
+          <ul className="main-menu">
+            <li>
+              <a href="">
+                <center>
+                  <img src={logo} className="transparent-shadow" style={{ maxHeight: '150px' }} />
+                </center>
+              </a>
+            </li>
+            <li>
+            <hr className='mt-3' />
+            </li>
+            <li className="slide__category"><span className="category-name">Main</span></li>
+            <li className="slide">
+              <Link to="/" className="side-menu__item">
+                <i className="w-6 h-4 side-menu__icon bi bi-speedometer"></i>
+                <span className="side-menu__label">
+                  Dashboard &ensp;
+                  <span className=" translate-middle badge !rounded-full bg-danger"> 5+ </span>
+                </span>
+              </Link>
+            </li>
+            <li className="slide">
+              <Link to='/customers' className="side-menu__item">
+                <i className="w-6 h-4 side-menu__icon bi bi-people-fill"></i>
+                <span className="side-menu__label">
+                  Manage Customers
+                </span>
+              </Link>
+            </li>
+            <li className="slide">
+              <Link to='/students' className="side-menu__item">
+                <i className="w-6 h-4 side-menu__icon bi bi-mortarboard-fill"></i>
+                <span className="side-menu__label">
+                  Manage Student
+                </span>
+              </Link>
+            </li>
+            <li className="slide">
+              <Link to='/employees' className="side-menu__item">
+                <i className="w-6 h-4 side-menu__icon bi bi-person-fill-gear"></i>
+                <span className="side-menu__label">
+                  Manage Employees
+                </span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </aside>
+</>
+)
+}
+
+export default Sidemenu
---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+import './assets/css/style.css'
+import Dashboard from './pages/dashboard.tsx'
+import Customer_Registration from './pages/customer/register.tsx';
+import Customer_List from './pages/customer/list.tsx';
+import Employee_Registration from './pages/employee/register.tsx';
+import Employee_List from './pages/employee/list.tsx';
+import Student_Registration from './pages/student/register.tsx';
+import Student_List from './pages/student/list.tsx';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/customers" element={<Customer_List />} />
+        <Route path="/customer/create" element={<Customer_Registration />} />
+        <Route path="/employees" element={<Employee_List />} />
+        <Route path="/employee/create" element={<Employee_Registration />} />
+        <Route path="/students" element={<Student_List />} />
+        <Route path="/student/create" element={<Student_Registration />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
